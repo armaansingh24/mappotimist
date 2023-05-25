aa8fbbc451400640a7d1f6bc617e95b670bf596e
Armaan Singh <sarmaan20720@gmail.com>
2023-05-26 00:21:16 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
-import './components/Component.css'
+// import './components/Component.css'
 
 const Home = () => {
   return (
     <div className=" w-full flex justify-center items-center text-black text-3xl my-auto mx-auto mt-20 sm:mt-1 p-2 ">
       <HeroSection />
-      <div class="waves">
+      {/* <div class="waves">
         <div class="wave wave1"></div>
-      </div>
+      </div> */}
     </div>
   );
 };
