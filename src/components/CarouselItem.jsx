4dab3d994e7d4d2b372eea42cf015a8e262897d9
Armaan Singh <sarmaan20720@gmail.com>
2023-05-29 00:40:19 +0530

    second commit

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -11,8 +11,8 @@ const CarouselItem = ({ item }) => {
       />
       <div className="absolute inset-0">
         <div className="w-full  flex justify-between p-2 sm:p-0 sm:px-10 sm:py-4 items-center gap-3">
-          <div className="my-2 sm:my-0 text-left md:w-full flex md:gap-3 flex-col">
-            <p className="text-sm sm:text-xl font-semibold md:text-2xl ">
+          <div className="my-2 sm:my-0 text-left md:w-full flex md:gap-3 flex-col ">
+            <p className="text-sm sm:text-xl font-semibold md:text-2xl md:">
               {item.step}
             </p>
             <p className="text-lg sm:text-2xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
