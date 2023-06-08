740876676b36363286e39ffa1bda3efed8c789bd
Armaan Singh <sarmaan20720@gmail.com>
2023-06-08 21:52:44 +0530

    footer

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import { useTypewriter,Cursor } from "react-simple-typewriter";  
 
 const ContactSection = () => {
+
+  const [ text ] = useTypewriter({
+    words: ['Project?','Idea?'],
+    loop:{},
+    typeSpeed: 200,
+    delaySpeed: 200,
+  });
   return (
     <>
-      <div className="relative z-30">
-        <div className="w-[90%] mx-auto p-10">
-          <h2 className="font-Poppins font-semibold text-[30px] lmd:text-[48px] flex gap-2 flex-wrap">
+      <div className="relative z-30 w-full -mt-10">
+        <div className=" mx-auto p-3">
+          <h2 className="font-Poppins font-semibold text-[30px] lmd:text-[48px] text-start">
             Contact Us
           </h2>
+          <div className="flex flex-col">
+            <span>Hey there!</span>
+            <div>
+              <span>Do you have a {' '}</span>
+              <span className="text-[#DF9507] font-bold lmd:text-[48px] -z-1 inline">
+                {text}
+                <Cursor cursorStyle="." />
+              </span>
+            </div>
+            <span className="">Let's Talk 👋</span>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default ContactSection
+export default ContactSection;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,20 +11,24 @@ const Footer = () => {
         <div className="w-[90%] mx-auto flex h-full p-0">
           {/* <p className=''>MappOptimist</p> */}
           <div className="mt-10 text-white flex flex-col h-[80%] gap-20 items-end w-full">
-            <div className="flex flex-col gap-10 w-full">
-              <p className="text-2xl">MappOptimist</p>
-              <ul className="text-xl flex gap-1 flex-col">
+            <div className="flex flex-col gap-10 w-full sm:flex-row sm:gap-0 sm:mr-20 midxs:justify-around midxs:mr-40">
+              <p className="text-2xl sm:text-3xl">MappOptimist</p>
+              <ul className="text-xl flex gap-1 flex-col sm:text-2xl">
                 <li className="cursor-pointer">about</li>
                 <li className="cursor-pointer">careers</li>
                 <li className="cursor-pointer">privacy policies</li>
                 <li className="cursor-pointer">terms & conditions</li>
               </ul>
             </div>
-            <div className="flex gap-4 self-end justify-items-end">
-              <img src={fb} alt=""  className="cursor-pointer" />
-              <img src={slack} alt="" className="cursor-pointer" />
-              <img src={insta} alt=""  className="cursor-pointer"/>
-              <img src={linkedin} alt="" className="cursor-pointer" />
+            <div className="flex gap-4 self-end justify-items-end midxs:mt-10">
+              <img src={fb} alt="" className="cursor-pointer h-[67%] sm:h-[90%] sm:w-[80%]" />
+              <img src={slack} alt="" className="cursor-pointer h-[67%] sm:h-[90%] sm:w-[80%]" />
+              <img src={insta} alt="" className="cursor-pointer h-[67%] sm:h-[90%] sm:w-[80%]" />
+              <img
+                src={linkedin}
+                alt=""
+                className="cursor-pointer h-[66%] sm:h-[89%] sm:w-[80%]"
+              />
             </div>
           </div>
         </div>
