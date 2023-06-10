218559ac2d3be8f37c017da5acce1a9caf266f7d
Armaan Singh <sarmaan20720@gmail.com>
2023-06-10 23:47:00 +0530

    client reviews

--- src/components/ClientReviewsItem.jsx
+++ src/components/ClientReviewsItem.jsx
@@ -7,7 +7,7 @@ const ClientReviewsItem = ({item}) => {
         <div className="text-[80%] sm:text-[100%] leading-relaxed mb-20 w-full">
           <p>"{item.description}"</p>
         </div>
-        <div className="w-full bg-[#D9D9D9] flex rounded-2xl gap-10 xsm:gap-5 items-start">
+        <div className="w-full bg-[#D9D9D9] flex rounded-2xl gap-10 xsm:gap-5 justify-start">
           <img src={item.image} alt="" className="w-[25%] rounded-2xl object-cover h-[25%]" />
           <div className="w-full flex flex-col items-start justify-center">
             <p className="text-2xl sm:text-3xl font-bold ml-2">{item.name}</p>
