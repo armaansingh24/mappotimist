bd14774741e31c1d0808096ae59908004b06e459
Armaan Singh <sarmaan20720@gmail.com>
2023-06-10 22:41:25 +0530

    agencies

--- src/components/Agencies.jsx
+++ src/components/Agencies.jsx
@@ -6,14 +6,14 @@ import wave1 from "../assets/agencies/wave1.png";
 const Agencies = () => {
   return (
     <>
-      <div className=" relative object-contain bg-no-repeat w-screen agencies bg-[url('https://img.freepik.com/free-photo/abstract-blue-geometric-shapes-background_24972-1841.jpg?w=1060&t=st=1684932147~exp=1684932747~hmac=d2625ef7bd40079dd5e0f224a61318a2cf6f7efd69ba12f788fa28c8a6900f9f')] agencies  z-10">
+      <div className=" relative object-contain bg-no-repeat w-screen agencies bg-[url('https://img.freepik.com/free-photo/abstract-blue-geometric-shapes-background_24972-1841.jpg?w=1060&t=st=1684932147~exp=1684932747~hmac=d2625ef7bd40079dd5e0f224a61318a2cf6f7efd69ba12f788fa28c8a6900f9f')] agencies overflow-hidden z-10">
         <img
           src={wave1}
           alt=""
           className="absolute top-[-30rem] left-[7rem] h-full scale-[2] hidden xxmd:block z-0 select-none"
         />
-        <div className="w-full ssm:w-[79%] mx-auto msm:mt-10 h-auto z-10 relative">
-          <h2 className="text-center font-Poppins font-semibold text-lg msm:text-[30px] lmd:text-[48px] flex gap-2 flex-wrap h-[10rem] z-10 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary xxmd:text-5xl xxmd:w-[40%] select-none">
+        <div className="w-[79%] mx-auto  mt-10 h-auto z-30 relative">
+          <h2 className="font-Poppins font-semibold text-[30px] lmd:text-[48px] flex gap-2 flex-wrap h-[10rem] z-10 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary xxmd:text-5xl xxmd:w-[40%] select-none">
             Available On Top Rated Agencies
           </h2>
         </div>
