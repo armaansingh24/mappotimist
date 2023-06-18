9307279d606d8c1e811a7897ca70db5791dc6e94
Armaan Singh <sarmaan20720@gmail.com>
2023-06-19 00:44:09 +0530

    carausel

--- src/components/PortfolioSlider.jsx
+++ src/components/PortfolioSlider.jsx
@@ -42,7 +42,7 @@ const PortfolioSlider = ({ image, index, currentSlideIndex,item }) => {
             index === currentSlideIndex
               ? "group-hover:opacity-[1] group-hover:bg-[rgb(0,0,0,0.7)]"
               : ""
-          } h-[35rem] mt-1 absolute inset-0 transition-all delay-75 ease-in-out rounded-[3rem] overflow-hidden w-full xl:w-[64%] midXl:w-[59.3%] xxl:w-[58.6%] midxmd:ml-[5.3rem]`}
+          } h-[35rem] mt-1 absolute inset-0 transition-all delay-75 ease-in-out rounded-[3rem] overflow-hidden w-full xl:w-[64%] midXl:w-[59.5%] xxl:w-[58.6%] midxmd:ml-[5.3rem]`}
         >
           <div className="w-[80%] xxmd:w-[90%] h-[90%] md:h-auto flex items-center justify-center flex-col mx-auto md:p-[3.4rem] xxmd:p-5 midxmd:p-2 border-white border-2 rounded-2xl mt-[10%] md:mt-[16%] xxmd:mt-[40.1%] gap-4 overflow-hidden">
             <motion.div
