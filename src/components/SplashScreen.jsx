1040d4508505321cf7e817d5d97313b9f0a44cbd
Armaan Singh <sarmaan20720@gmail.com>
2023-07-01 21:44:16 +0530

    code

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import logo from "../assets/navbar/logo.webp";
+import SplashLogo from "../assets/SplashLogo.png";
 
 function SplashScreen() {
   useEffect(() => {
@@ -15,7 +15,7 @@ function SplashScreen() {
   return (
     <div className="splash-screen bg-gradient-to-br overflow-hidden from-secondary to-secondary via-white w-screen h-screen flex items-center justify-center">
       <div className="logo overflow-hidden">
-        <img src={logo} alt="Logo" className="w-[30%] mx-auto" />
+        <img src={SplashLogo} alt="Logo" className="w-[30%] mx-auto" />
       </div>
     </div>
   );
