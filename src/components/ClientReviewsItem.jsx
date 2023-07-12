517f9162a633b5b78924b6c233d50d5208da70fb
Armaan Singh <sarmaan20720@gmail.com>
2023-07-13 01:11:14 +0530

    code

--- src/components/ClientReviewsItem.jsx
+++ src/components/ClientReviewsItem.jsx
@@ -4,7 +4,7 @@ const ClientReviewsItem = ({ item }) => {
   return (
     <>
       <div className="w-[90%] sm:w-[70%] rounded-lg h-full flex flex-col items-stretch justify-between">
-        <div className="text-[80%] sm:text-[100%] leading-relaxed mb-20 w-full font-poppins300">
+        <div className="text-[80%] sm:text-[98%] leading-relaxed mb-20 w-full font-poppins300">
           <p>"{item.description}"</p>
         </div>
         <div className="w-full bg-[#D9D9D9] flex rounded-2xl gap-5 justify-start">
