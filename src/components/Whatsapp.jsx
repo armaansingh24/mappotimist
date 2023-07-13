3aecbc264a76ba8cf8ad5d59e70cec2ba57d11e4
Armaan Singh <sarmaan20720@gmail.com>
2023-07-14 00:57:38 +0530

    code

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -39,7 +39,7 @@ const Whatsapp = () => {
             <img
               src={whatsapp}
               alt=""
-              className="w-12 sm:w-20 flex justify-center items-center rounded-full fixed bottom-10 sm:bottom-20 left-2 sm:left-6 z-40 cursor-pointer"
+              className="w-12 sm:w-20 flex justify-center items-center fixed bottom-10 sm:bottom-20 left-2 sm:left-6 z-40 cursor-pointer"
             />
           </a>
         </div>
